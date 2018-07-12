d4bb3d8790930271868dd579dc2ce44708f8d02d
Bizuwork Melesse <biz@bizuworks-mbp.usca.ibm.com>
2018-07-12 16:58:03 -0700

    Remove old backendDev and backendOld files

--- api/api/backendDEV/urls.py
+++
@@ -1,7 +0,0 @@
-import views
-from django.conf.urls import url, include
-
-urlpatterns = [
-    url(r'^speech_to_text/', views.speech_to_text, name='speech_to_text'),
-    url(r'^tone/', views.tone, name='tone'),
-]
