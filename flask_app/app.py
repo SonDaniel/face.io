54f82995228e331feb2f051070b5b6d2f12e1c84
Bizuwork Melesse <biz@bizuworks-mbp.usca.ibm.com>
2018-07-25 10:09:33 -0700

    Send binary data to the client

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, request, jsonify 
+from flask import Flask, render_template, request, jsonify, send_file 
+import requests
+import io
 app = Flask(__name__)
  
 @app.route('/')
 def render_static():
     return render_template('index.html')
 
+@app.route('/download_image/', methods=['GET'])
+def download_image():
+    image_url = request.args.get('image_url')
+    image_data = requests.get(image_url).content
+    return send_file(
+                     io.BytesIO(image_data),
+                     attachment_filename='yoda.jpeg',
+                     mimetype='image/jpg'
+    ) 
+
+
 @app.route('/image_endpoint', methods=['POST'])
 def image_endpoint():
-    return jsonify({"name": request.get_data()})
+    return jsonify({"yoda_binary": request.get_data()})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
